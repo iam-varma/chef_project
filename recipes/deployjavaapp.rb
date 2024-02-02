46344cd5b29a46f34e77b872ca7bf4e126c9f2be
akhil <149268110+iam-varma@users.noreply.github.com>
2024-02-03 01:16:29 +0530

    Update deployjavaapp.rb

--- recipes/deployjavaapp.rb
+++ recipes/deployjavaapp.rb
@@ -1,5 +1,5 @@
 remote_file '/opt/tomcat/webapps/login.war' do
-    source 'https://mymahas326aug23.s3.us-east-2.amazonaws.com/login.war'
+    source 'your warfile adress'
     mode '0755'
     action :create
 end
